8416b6bdfa9b78ce86d8690a51761c0ad7bdb843
“fudj1” <fyudjfhuh@gmail.com>
2026-04-05 20:05:03 +0500

    feat: module2-task1

--- src/presenter.js
+++ src/presenter.js
@@ -6,79 +6,44 @@ import TripEventsList from './view/tripEventsList.js';
 import { render, RenderPosition } from './render.js';
 
 export default class Presenter {
-  constructor() {
+  constructor(model) {
+    this.model = model;
     this.filter = new Filter();
     this.sort = new Sort();
-    this.eventForm = new EventForm({
-      destination: 'Geneva',
-      startTime: '19/03/19 00:00',
-      endTime: '19/03/19 00:00',
-      price: ''
-    });
     this.tripEventsList = new TripEventsList();
-
-    this.events = [
-      {
-        date: 'MAR 18',
-        dateTime: '2019-03-18',
-        typeIcon: 'taxi',
-        title: 'Taxi Amsterdam',
-        startTime: '10:30',
-        endTime: '11:00',
-        startDateTime: '2019-03-18T10:30',
-        endDateTime: '2019-03-18T11:00',
-        duration: '30M',
-        price: '20',
-        offers: [
-          { title: 'Order Uber', price: '20' }
-        ]
-      },
-      {
-        date: 'MAR 18',
-        dateTime: '2019-03-18',
-        typeIcon: 'flight',
-        title: 'Flight Chamonix',
-        startTime: '12:25',
-        endTime: '13:35',
-        startDateTime: '2019-03-18T12:25',
-        endDateTime: '2019-03-18T13:35',
-        duration: '01H 10M',
-        price: '160',
-        offers: []
-      },
-      {
-        date: 'MAR 18',
-        dateTime: '2019-03-18',
-        typeIcon: 'drive',
-        title: 'Drive Chamonix',
-        startTime: '14:30',
-        endTime: '16:30',
-        startDateTime: '2019-03-18T14:30',
-        endDateTime: '2019-03-18T16:30',
-        duration: '02H',
-        price: '50',
-        offers: []
-      }
-    ];
   }
 
   init() {
     const filterContainer = document.querySelector('.trip-controls__filters');
     const eventsSection = document.querySelector('.trip-events');
+    const tripList = document.querySelector('.trip-events__list');
 
     render(this.filter, filterContainer);
     render(this.sort, eventsSection, RenderPosition.BEFOREEND);
     render(this.tripEventsList, eventsSection, RenderPosition.BEFOREEND);
 
-    const tripList = document.querySelector('.trip-events__list');
+    const points = this.model.getPoints();
+    const destinations = this.model.getDestinations();
+    const offers = this.model.getOffers();
 
-    const eventFormItem = document.createElement('li');
-    eventFormItem.classList.add('trip-events__item');
-    eventFormItem.appendChild(this.eventForm.getElement());
-    tripList.appendChild(eventFormItem);
+    if (points.length > 0) {
+      const firstPoint = points[0];
+      const firstDestination = this.model.getDestinationById(firstPoint.destinationId);
+      const firstPointOffers = this.model.getOffersByIds(firstPoint.offers);
+      const availableOffers = this.model.getOffersByType(firstPoint.type);
 
-    this.events.forEach(eventData => {
-      const event = new Event(eventData);
+      const eventForm = new EventForm(firstPoint, firstDestination, availableOffers);
+      const eventFormItem = document.createElement('li');
+      eventFormItem.classList.add('trip-events__item');
+      eventFormItem.appendChild(eventForm.getElement());
+      tripList.appendChild(eventFormItem);
+    }
+
+    points.forEach(point => {
+      const destination = this.model.getDestinationById(point.destinationId);
+      const pointOffers = this.model.getOffersByIds(point.offers);
+
+      const event = new Event(point, destination, pointOffers);
       const eventItem = document.createElement('li');
       eventItem.classList.add('trip-events__item');
       eventItem.appendChild(event.getElement());
@@ -86,6 +51,3 @@ export default class Presenter {
     });
   }
 }
-
-
-// надеюсь коммит сработает
